8f951832d170ce0d196392541f8eb203d33cf27d
balt-dev <heptor42+git@gmail.com>
2025-08-15 16:19:37 -0500

    todo texture

--- Textures/progress.py
+++ Textures/progress.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from PIL import Image
 
 def main():
+	with open("todo.png", "rb") as todo:
+		todo_data = todo.read()
+
 	total_count = 0
 	small_count = 0
 	for path in Path(".").rglob("*.png"):
@@ -11,13 +14,19 @@ def main():
 		try:
 			with Image.open(path) as im:
 				if im.width <= 64 and im.height <= 32:
-					small_count += 1
 					small = True
+			if small:
+				small = False
+				with open(path, "rb") as p:
+					if p.read() != todo_data:
+						small = True
 		except:
 			pass
-		if not small:
-			with open(path, "w") as f:
-				pass
+		if small:
+			small_count += 1
+		else:
+			with open(path, "wb") as f:
+				f.write(todo_data)
 				
 	print(f"{(small_count / total_count * 100):.2f}% done ({small_count} / {total_count})")
 
